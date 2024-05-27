bc699848ec230ab41df6effdb7ef4e888d9e0577
Qilqilll <aqilf612@gmail.com>
2024-05-27 10:08:35 +0700

    memasukan class seseorang

--- ParadigmaOOP_0145/ParadigmaOOP_0145.cpp
+++ ParadigmaOOP_0145/ParadigmaOOP_0145.cpp
@@ -8,29 +8,3 @@ public:
 	//cout <<"pesan daari seseorang" << endl;
 	//}
 };
-
-class joko :public seseorang {
-	public:
-		void pesan() {
-			cout << "pesan dari joko" << endl;
-		}
-};
-class lia :public seseorang {
-public:
-	void pesan() {
-		cout << "pesan dari lia" << endl;
-	}
-};
-
-int main() {
-	seseorang* obyek;
-	joko a;
-	lia b;
-
-	obyek = &a;
-	obyek->pesan();
-	obyek = &b;
-	obyek->pesan();
-
-	return 0;
-}
