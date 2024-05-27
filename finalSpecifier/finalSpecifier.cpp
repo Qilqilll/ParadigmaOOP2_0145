c2b55758d2cefd350d5a0a7533ccdca44da99d1d
Qilqilll <aqilf612@gmail.com>
2024-05-27 10:29:18 +0700

    darivedClass and baseClass (public)

--- finalSpecifier/finalSpecifier.cpp
+++ finalSpecifier/finalSpecifier.cpp
@@ -7,3 +7,10 @@ public:
 		cout << "hallo saya function dari base class";
 	}
 };
+class darivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "hallo saya function dari base class";
+
+	}
+};
